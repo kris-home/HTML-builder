0ba543a120f18e2a1e440bc8b90fc8d5799399d4
Kristina Homenkova <kristina.khomenkova@gmail.com>
2024-01-21 23:46:28 +0300

    feat: fifth task completed

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const fsPromises = require('fs/promises');
+const path = require('path');
+let arr = [];
+
+fs.readdir(path.join(__dirname, 'styles'), { withFileTypes: true }, (error, files) => {
+  files.forEach((file) => {
+    if (file.isFile && path.extname(file.name) === '.css') {
+      fs.readFile(path.join(__dirname, 'styles', file.name), (err, data) => {
+        arr.push(data);
+        fs.unlink(path.join(__dirname, 'project-dist', 'bundle.css'), (err) => {
+          fs.appendFile(path.join(__dirname, 'project-dist', 'bundle.css'), data, (err) => {
+          })
+        });
+      });
+    }
+  });
+  console.log('Merging .css files from "styles" folder completed successfully')
+}
+)
