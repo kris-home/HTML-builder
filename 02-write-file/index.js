40a845cf07255dc5ea1e29119afcb1bd2dde36c9
Kristina Homenkova <kristina.khomenkova@gmail.com>
2024-01-22 20:39:28 +0300

    fix: second task fixed

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,15 +3,13 @@ const path = require('path');
 const { stdin, stdout } = process;
 
 stdout.write('Hello!\nEnter your text\n');
+fs.appendFile(path.join(__dirname, '02-write-file.txt'), '', (err) => {
+  });
 stdin.on('data', (data) => {
-  fs.appendFile(
-    path.join(__dirname, '02-write-file.txt'),
-    data.toString(),
-    (err) => {
-      if (err) throw err;
-    },
-  );
-});
+  if (data.toString().includes('exit')) process.exit();
+  fs.appendFile(path.join(__dirname, '02-write-file.txt'), data.toString(), (err) => {
+    })
+  });
 
 process.on('exit', () => {
   console.log('Goodbye!')
