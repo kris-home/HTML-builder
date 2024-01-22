3b2c475e7f17fb791e96f6d44c82d40974f1a911
Kristina Homenkova <kristina.khomenkova@gmail.com>
2024-01-22 21:17:25 +0300

    fix: fifth task fixed

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,12 +1,10 @@
 const fs = require('fs');
 const path = require('path');
-let arr = [];
 
 fs.readdir(path.join(__dirname, 'styles'), { withFileTypes: true }, (error, files) => {
   files.forEach((file) => {
     if (file.isFile && path.extname(file.name) === '.css') {
       fs.readFile(path.join(__dirname, 'styles', file.name), (err, data) => {
-        arr.push(data);
         fs.unlink(path.join(__dirname, 'project-dist', 'bundle.css'), (err) => {
           fs.appendFile(path.join(__dirname, 'project-dist', 'bundle.css'), data, (err) => {
           })
